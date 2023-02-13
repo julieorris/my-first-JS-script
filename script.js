978b310024a0875a4c5692ac3f8a4ab6d1d2bae7
Julie O <119204880+julieorris@users.noreply.github.com>
2023-02-13 09:06:09 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Write your JavaScript below this line
+var name = "Julie";
+var favoriteSnack = "french fries";
+console.log(
+  "Hi, my name is " + name + "and I wish I had " + favoriteSnack + " right now."
+);
+
+var numberOfHorses = "15";
+console.log("The number of horses on the farm was " + numberOfHorses + ".");
+
+var favoriteFlower = "Calilily";
+console.log("My favorite flower is " + favoriteFlower + " .");
+
+var birthCity = "Campbell, CA";
+var birthday = "April 12, 1988";
+console.log("I was born in " + birthCity + " on " + birthday + " .");
